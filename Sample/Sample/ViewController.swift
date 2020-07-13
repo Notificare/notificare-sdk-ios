ab3468ff9542fb5dddec149b505f930f25ffebee
Helder Pinhal <helder@notifica.re>
2020-07-13 14:11:57 +0200

    feat: add sample application

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Helder Pinhal on 13/07/2020.
+//  Copyright © 2020 Notificare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
