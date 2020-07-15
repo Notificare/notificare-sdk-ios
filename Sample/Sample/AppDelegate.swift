e84e6ff90f4ffdf55a50686305f6d67dd3974860
Helder Pinhal <helder@notifica.re>
2020-07-15 10:59:15 +0200

    feat(sample): use autoLaunch

--- Sample/Sample/AppDelegate.swift
+++ Sample/Sample/AppDelegate.swift
@@ -7,17 +7,12 @@
 //
 
 import UIKit
-import Notificare
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
-        Notificare.shared.logger.level = .verbose
-        Notificare.shared.launch()
-
         return true
     }
 
