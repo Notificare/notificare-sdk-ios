9b8a998b6e4e977c6ebbcbfbe67c1d6a83569cd9
Helder Pinhal <helder@notifica.re>
2020-12-23 11:59:19 +0000

    chore: update sample with push delegate

--- Sample/Sample/AppDelegate.swift
+++ Sample/Sample/AppDelegate.swift
@@ -8,7 +8,7 @@ import NotificarePushKit
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, NotificareDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, NotificareDelegate {
         Notificare.shared.logger.level = .verbose
         Notificare.shared.delegate = self
 
+        NotificarePush.shared.delegate = self
+
         // Enable Proxyman debugging.
         Atlantis.start()
 
@@ -24,21 +26,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate, NotificareDelegate {
     }
 
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken token: Data) {
-        print("-----> didRegisterForRemoteNotificationsWithDeviceToken: \(token.toHexString())")
+        print("-----> didRegisterForRemoteNotificationsWithDeviceToken")
         NotificarePush.shared.application(application, didRegisterForRemoteNotificationsWithDeviceToken: token)
     }
 
     func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        print("-----> didFailToRegisterForRemoteNotificationsWithError: \(error)")
+        print("-----> didFailToRegisterForRemoteNotificationsWithError")
         NotificarePush.shared.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
     }
 
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        print("-----> didReceiveRemoteNotification: \(userInfo)")
+        print("-----> didReceiveRemoteNotification")
         NotificarePush.shared.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
     }
+}
 
-    // MARK: - NotificareDelegate
+extension AppDelegate: NotificareDelegate {
 
     func notificare(_: Notificare, onReady _: NotificareApplication) {
         print("-----> Notificare is ready.")
@@ -46,5 +49,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate, NotificareDelegate {
 
     func notificare(_: Notificare, didRegisterDevice device: NotificareDevice) {
         print("-----> Notificare: device registered: \(device)")
+    }
+}
+
+extension AppDelegate: NotificarePushDelegate {
+
+    func notificare(_ notificarePush: NotificarePush, didChangeNotificationSettings granted: Bool) {
+        print("-----> Notificare: notification settings changed: \(granted)")
     }
 }
