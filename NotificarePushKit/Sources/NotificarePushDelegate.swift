eeb4e1fe1b85ca926fd820fd99cb9145087da414
Helder Pinhal <helder@notifica.re>
2020-12-23 18:59:15 +0000

    feat(push): optional delegate method
    ISSUE SDK-7

--- NotificarePushKit/Sources/NotificarePushDelegate.swift
+++ NotificarePushKit/Sources/NotificarePushDelegate.swift
@@ -7,3 +7,7 @@ import Foundation
 public protocol NotificarePushDelegate: AnyObject {
     func notificare(_ notificarePush: NotificarePush, didChangeNotificationSettings granted: Bool)
 }
+
+extension NotificarePushDelegate {
+    func notificare(_: NotificarePush, didChangeNotificationSettings _: Bool) {}
+}
