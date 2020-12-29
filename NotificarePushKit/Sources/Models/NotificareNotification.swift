223f31ed2fa346ce8269dacac3e48148e7ebc082
Helder Pinhal <helder@notifica.re>
2020-12-29 21:58:05 +0000

    feat(push): handle all notification props
    ISSUE SDK-7

--- NotificarePushKit/Sources/Models/NotificareNotification.swift
+++ NotificarePushKit/Sources/Models/NotificareNotification.swift
@@ -4,21 +4,23 @@
 
 import Foundation
 
-public struct NotificareNotification: Codable {
+public struct NotificareNotification {
     public let id: String?
     // public let application: Dictionary
     public let type: String
-    public let time: String
+    public let time: Date
     public let title: String?
     public let subtitle: String?
     public let message: String
-    // public let content: [NotificareContent]?
-    // public let actions: [NotificareAction]?
-    // public let attachments: [NotificareAttachment]?
-    // public let extra: Dictionary?
-    // public let info: Dictionary?
+    public let content: [Content]
+    public let actions: [Action]
+    public let attachments: [Attachment]
+    public let extra: [String: Any]
+    public let info: [String: Any]
     public let targetContentIdentifier: String?
+}
 
+extension NotificareNotification: Decodable {
     enum CodingKeys: String, CodingKey {
         case id = "_id"
         case type
@@ -26,6 +28,97 @@ public struct NotificareNotification: Codable {
         case title
         case subtitle
         case message
+        case content
+        case actions
+        case attachments
+        case extra
+        case info
         case targetContentIdentifier
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decodeIfPresent(String.self, forKey: .id)
+        type = try container.decode(String.self, forKey: .type)
+        time = try container.decode(Date.self, forKey: .time)
+        title = try container.decodeIfPresent(String.self, forKey: .title)
+        subtitle = try container.decodeIfPresent(String.self, forKey: .subtitle)
+        message = try container.decode(String.self, forKey: .message)
+
+        if container.contains(.content) {
+            content = try container.decode([Content].self, forKey: .content)
+        } else {
+            content = []
+        }
+
+        if container.contains(.actions) {
+            actions = try container.decode([Action].self, forKey: .actions)
+        } else {
+            actions = []
+        }
+
+        if container.contains(.attachments) {
+            attachments = try container.decode([Attachment].self, forKey: .attachments)
+        } else {
+            attachments = []
+        }
+
+        if container.contains(.extra) {
+            extra = try container.decode([String: Any].self, forKey: .extra)
+        } else {
+            extra = [:]
+        }
+
+        if container.contains(.info) {
+            info = try container.decode([String: Any].self, forKey: .info)
+        } else {
+            info = [:]
+        }
+
+        targetContentIdentifier = try container.decodeIfPresent(String.self, forKey: .targetContentIdentifier)
+    }
+}
+
+// NotificareNotification.Content
+public extension NotificareNotification {
+    struct Content: Decodable {
+        public let type: String
+        public let data: Any
+
+        public init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            type = try container.decode(String.self, forKey: .type)
+
+            if let str = try? container.decode(String.self, forKey: .data) {
+                data = str
+            } else {
+                data = try container.decode([String: Any].self, forKey: .data)
+            }
+        }
+
+        enum CodingKeys: String, CodingKey {
+            case type
+            case data
+        }
+    }
+}
+
+// NotificareNotification.Action
+public extension NotificareNotification {
+    struct Action: Decodable {
+        public let type: String
+        public let label: String
+        public let target: String?
+        public let keyboard: Bool
+        public let camera: Bool
+    }
+}
+
+// NotificareNotification.Attachment
+public extension NotificareNotification {
+    struct Attachment: Decodable {
+        public let mimeType: String
+        public let uri: String
     }
 }
