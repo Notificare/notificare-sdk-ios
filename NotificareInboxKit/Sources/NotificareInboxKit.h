5a94198f19068f07a579bafd96ee60073ff4cc5a
Helder Pinhal <helder@notifica.re>
2021-01-20 13:17:53 +0000

    chore: add missing header

---
+++ NotificareInboxKit/Sources/NotificareInboxKit.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2021 Notificare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NotificareInboxKit.
+FOUNDATION_EXPORT double NotificareInboxKitVersionNumber;
+
+//! Project version string for NotificareInboxKit.
+FOUNDATION_EXPORT const unsigned char NotificareInboxKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificareInboxKit/PublicHeader.h>
+
+
