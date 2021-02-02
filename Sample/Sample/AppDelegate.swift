8bbdbfd84c2b0f569b8e42a0ef136da908c5eab2
Helder Pinhal <helder@notifica.re>
2021-02-02 10:47:51 +0000

    chore: update sample

--- Sample/Sample/AppDelegate.swift
+++ Sample/Sample/AppDelegate.swift
@@ -3,7 +3,6 @@
 //
 
 import Atlantis
-import NotificareCore
 import NotificareKit
 import NotificarePushKit
 import NotificarePushUIKit
@@ -20,8 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Enable Proxyman debugging.
         Atlantis.start()
 
-        NotificareLogger.useAdvancedLogging = true
-
+        Notificare.shared.useAdvancedLogging = true
+        
         if #available(iOS 14.0, *) {
             NotificarePush.shared.presentationOptions = [.banner, .badge, .sound]
         } else {
@@ -52,42 +51,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 extension AppDelegate: NotificareDelegate {
 
     func notificare(_: Notificare, onReady _: NotificareApplication) {
-        NotificareLogger.info("-----> Notificare is ready.")
+        print("-----> Notificare is ready.")
     }
 
     func notificare(_: Notificare, didRegisterDevice device: NotificareDevice) {
-        NotificareLogger.info("-----> Notificare: device registered: \(device)")
+        print("-----> Notificare: device registered: \(device)")
     }
 }
 
 extension AppDelegate: NotificarePushDelegate {
 
     func notificare(_ notificarePush: NotificarePush, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-        NotificareLogger.info("-----> Notificare: failed to register for remote notifications: \(error)")
+        print("-----> Notificare: failed to register for remote notifications: \(error)")
     }
 
     func notificare(_ notificarePush: NotificarePush, didChangeNotificationSettings granted: Bool) {
-        NotificareLogger.info("-----> Notificare: notification settings changed: \(granted)")
+        print("-----> Notificare: notification settings changed: \(granted)")
     }
 
     func notificare(_ notificarePush: NotificarePush, didReceiveSystemNotification notification: NotificareSystemNotification) {
-        NotificareLogger.info("-----> Notificare: received a system notification: \(notification)")
+        print("-----> Notificare: received a system notification: \(notification)")
     }
 
     func notificare(_ notificarePush: NotificarePush, didReceiveNotification notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: received a notification: \(notification)")
+        print("-----> Notificare: received a notification: \(notification)")
     }
 
     func notificare(_ notificarePush: NotificarePush, didReceiveUnknownNotification userInfo: [AnyHashable : Any]) {
-        NotificareLogger.info("-----> Notificare: received an unknown notification: \(userInfo)")
+        print("-----> Notificare: received an unknown notification: \(userInfo)")
     }
 
     func notificare(_ notificarePush: NotificarePush, shouldOpenSettings notification: NotificareNotification?) {
-        NotificareLogger.info("-----> Notificare: should open notification settings")
+        print("-----> Notificare: should open notification settings")
     }
 
     func notificare(_ notificarePush: NotificarePush, didReceiveUnknownAction action: [AnyHashable : Any], for notification: [AnyHashable : Any]) {
-        NotificareLogger.info("-----> Notificare: received an unknown action: \(action)")
+        print("-----> Notificare: received an unknown action: \(action)")
     }
     
     func notificare(_ notificarePush: NotificarePush, didOpenNotification notification: NotificareNotification) {
@@ -109,39 +108,39 @@ extension AppDelegate: NotificarePushDelegate {
 
 extension AppDelegate: NotificarePushUIDelegate {
     func notificare(_ notificarePushUI: NotificarePushUI, willPresentNotification notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: will present notification '\(notification.id)'")
+        print("-----> Notificare: will present notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, didPresentNotification notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: did present notification '\(notification.id)'")
+        print("-----> Notificare: did present notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, didFailToPresentNotification notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: did fail to present notification '\(notification.id)'")
+        print("-----> Notificare: did fail to present notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, didFinishPresentingNotification notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: did finish presenting notification '\(notification.id)'")
+        print("-----> Notificare: did finish presenting notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, didClickURL url: URL, in notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: did click url '\(url)' in notification '\(notification.id)'")
+        print("-----> Notificare: did click url '\(url)' in notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, willExecuteAction action: NotificareNotification.Action, for notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: will execute action '\(action.label)' in notification '\(notification.id)'")
+        print("-----> Notificare: will execute action '\(action.label)' in notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, didExecuteAction action: NotificareNotification.Action, for notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: did execute action '\(action.label)' in notification '\(notification.id)'")
+        print("-----> Notificare: did execute action '\(action.label)' in notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, didNotExecuteAction action: NotificareNotification.Action, for notification: NotificareNotification) {
-        NotificareLogger.info("-----> Notificare: did not execute action '\(action.label)' in notification '\(notification.id)'")
+        print("-----> Notificare: did not execute action '\(action.label)' in notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, didFailToExecuteAction action: NotificareNotification.Action, for notification: NotificareNotification, error: Error?) {
-        NotificareLogger.info("-----> Notificare: did fail to execute action '\(action.label)' in notification '\(notification.id)'")
+        print("-----> Notificare: did fail to execute action '\(action.label)' in notification '\(notification.id)'")
     }
     
     func notificare(_ notificarePushUI: NotificarePushUI, shouldPerformSelectorWithURL url: URL, in action: NotificareNotification.Action, for notification: NotificareNotification) {
@@ -151,10 +150,10 @@ extension AppDelegate: NotificarePushUIDelegate {
 
 extension AppDelegate: NotificareInboxDelegate {
     func notificare(_ notificareInbox: NotificareInbox, didUpdateInbox items: [NotificareInboxItem]) {
-        NotificareLogger.info("-----> Inbox has loaded. Total = \(items.count)")
+        print("-----> Inbox has loaded. Total = \(items.count)")
     }
     
     func notificare(_ notificareInbox: NotificareInbox, didUpdateBadge badge: Int) {
-        NotificareLogger.info("-----> Badge update. Unread = \(badge)")
+        print("-----> Badge update. Unread = \(badge)")
     }
 }
