5ea9d633f6f6333097934117d87a7a1dc5bb533d
Helder Pinhal <helder@notifica.re>
2021-04-14 18:12:16 +0200

    chore: include built xcframeworks

---
+++ .build/NotificareKit.xcframework/ios-arm64_i386_x86_64-simulator/NotificareKit.framework/Headers/NotificareKit.h
@@ -0,0 +1,13 @@
+//
+// Copyright (c) 2020 Notificare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Notificare.
+FOUNDATION_EXPORT double NotificareVersionNumber;
+
+//! Project version string for Notificare.
+FOUNDATION_EXPORT const unsigned char NotificareVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificareSDK/PublicHeader.h>
---
+++ .build/NotificarePushKit.xcframework/ios-arm64_i386_x86_64-simulator/NotificarePushKit.framework/Headers/NotificarePushKit.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2020 Notificare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NotificarePush.
+FOUNDATION_EXPORT double NotificarePushVersionNumber;
+
+//! Project version string for NotificarePush.
+FOUNDATION_EXPORT const unsigned char NotificarePushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificarePush/PublicHeader.h>
+
+
