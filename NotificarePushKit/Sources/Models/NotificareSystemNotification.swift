e2a63c1b617df20e4e67e19249ef8849694507d0
Helder Pinhal <helder@notifica.re>
2021-04-23 14:55:34 +0200

    feat(push): update system notification model

--- NotificarePushKit/Sources/Models/NotificareSystemNotification.swift
+++ NotificarePushKit/Sources/Models/NotificareSystemNotification.swift
@@ -2,21 +2,56 @@
 // Copyright (c) 2020 Notificare. All rights reserved.
 //
 
-import Foundation
+import NotificareKit
 
 public struct NotificareSystemNotification {
-    public let id: String?
+    public let id: String
     public let type: String
-    public let extras: [AnyHashable: Any]
+    public let extra: [String: Any]
 
     init(userInfo: [AnyHashable: Any]) {
-        id = userInfo["id"] as? String
+        id = userInfo["id"] as! String
         type = userInfo["systemType"] as! String
 
-        let ignoreKeys = ["aps", "system", "systemType", "attachment", "notificationId", "id", "x-sender"]
-        extras = userInfo.filter { (entry) -> Bool in
-            let key = entry.key as? String ?? ""
-            return !ignoreKeys.contains(key)
-        }
+        let stringKeyedUserInfo = userInfo.filter { $0.key is String } as! [String: Any]
+        let ignoreKeys = ["aps", "system", "systemType", "attachment", "notificationId", "notificationType", "id", "x-sender"]
+
+        extra = stringKeyedUserInfo.filter { !ignoreKeys.contains($0.key) }
+    }
+
+    public func toJson() throws -> [String: Any] {
+        let data = try NotificareUtils.jsonEncoder.encode(self)
+        return try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+    }
+
+    public static func fromJson(json: [String: Any]) throws -> NotificareSystemNotification {
+        let data = try JSONSerialization.data(withJSONObject: json, options: [])
+        return try NotificareUtils.jsonDecoder.decode(NotificareSystemNotification.self, from: data)
+    }
+}
+
+extension NotificareSystemNotification: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id
+        case type
+        case extra
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decode(String.self, forKey: .id)
+        type = try container.decode(String.self, forKey: .type)
+
+        let decodedExtra = try container.decode(AnyCodable.self, forKey: .extra)
+        extra = decodedExtra.value as! [String: Any]
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(id, forKey: .id)
+        try container.encode(type, forKey: .type)
+        try container.encode(AnyCodable(extra), forKey: .extra)
     }
 }
