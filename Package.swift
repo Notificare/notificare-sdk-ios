b69a5a8ccff251f9627f2de8d34396ac77b74290
Helder Pinhal <helder@notifica.re>
2021-06-29 11:33:04 +0100

    chore: update package.swift

--- Package.swift
+++ Package.swift
@@ -31,22 +31,22 @@ let package = Package(
         .binaryTarget(
             name: "NotificareKit",
             url: "https://github.com/notificare/notificare-sdk-ios/releases/download/\(version)/NotificareKit.zip",
-            checksum: "f733c5a98609c98ddaec9938c4ee096b070162aacd02b5a5d9b64102fa1a942f"
+            checksum: "aaf82e40d99f0e5fef7ffbe3bc079447a6f1459e1b75165d8073e68fe6747969"
         ),
         .binaryTarget(
             name: "NotificareInboxKit",
             url: "https://github.com/notificare/notificare-sdk-ios/releases/download/\(version)/NotificareInboxKit.zip",
-            checksum: "172444e3118290e8c8f0e8c52a0aeaf116a0d554ba1e69e4d01c6dc77d38d700"
+            checksum: "4b7aeaf46d59bc5a2c22dddfd8220ce9393d2a41764119b79513335fea4408d8"
         ),
         .binaryTarget(
             name: "NotificarePushKit",
             url: "https://github.com/notificare/notificare-sdk-ios/releases/download/\(version)/NotificarePushKit.zip",
-            checksum: "d503151c8bd23abd892246dcdddbd02cb8b1ff7ad9a562697c9c2f20e3fcc37a"
+            checksum: "2a572af2a2ccd035d1deca9c1b8f904a9ea3f5cf2b9c27fb74209912171f7b1e"
         ),
         .binaryTarget(
             name: "NotificarePushUIKit",
             url: "https://github.com/notificare/notificare-sdk-ios/releases/download/\(version)/NotificarePushUIKit.zip",
-            checksum: "691bd7d68e1a5eefcf698d580c847ba3b94cc2c6177d293868f9c54180fbbd37"
+            checksum: "7ca0840aaf1c48b9b3658314a97056a946609cab9ccf61be942ce9172a255b77"
         ),
     ]
 )
