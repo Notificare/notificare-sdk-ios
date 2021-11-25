52ab90d567b00f438ddbca07dfac5651ba48ba34
Helder Pinhal <helder@notifica.re>
2021-11-25 18:40:52 +0000

    feat(scannables): add json methods

--- NotificareScannablesKit/Sources/Models/NotificareScannable.swift
+++ NotificareScannablesKit/Sources/Models/NotificareScannable.swift
@@ -11,3 +11,16 @@ public struct NotificareScannable: Codable {
     public let type: String
     public let notification: NotificareNotification?
 }
+
+// JSON: NotificareScannable
+public extension NotificareScannable {
+    func toJson() throws -> [String: Any] {
+        let data = try NotificareUtils.jsonEncoder.encode(self)
+        return try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+    }
+
+    static func fromJson(json: [String: Any]) throws -> NotificareScannable {
+        let data = try JSONSerialization.data(withJSONObject: json, options: [])
+        return try NotificareUtils.jsonDecoder.decode(NotificareScannable.self, from: data)
+    }
+}
