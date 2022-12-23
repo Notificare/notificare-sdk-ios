03ac542241d9eab6006f77ea3413c300bca7ee56
Helder Pinhal <helder@notifica.re>
2022-12-23 11:21:26 +0000

    feat(push): include the delivery mechanism for notification received events

---
+++ NotificarePushKit/Sources/Models/NotificareNotificationDeliveryMechanism.swift
@@ -0,0 +1,11 @@
+//
+// Copyright (c) 2022 Notificare. All rights reserved.
+//
+
+import Foundation
+import NotificareKit
+
+public enum NotificareNotificationDeliveryMechanism: String, Codable {
+    case standard
+    case silent
+}
