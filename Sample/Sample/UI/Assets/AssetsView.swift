f5ca3e7aa93d302def9ee1e93a8650b7200dc44a
Helder Pinhal <helder@notifica.re>
2023-05-17 11:11:29 +0100

    refactor(assets): update structure

--- Sample/Sample/UI/Assets/AssetsView.swift
+++ Sample/Sample/UI/Assets/AssetsView.swift
@@ -2,14 +2,11 @@
 // Copyright (c) 2023 Notificare. All rights reserved.
 //
 
+import NotificareAssetsKit
 import SwiftUI
 
 struct AssetsView: View {
-    @StateObject private var viewModel: AssetsViewModel
-
-    init() {
-        _viewModel = StateObject(wrappedValue: AssetsViewModel())
-    }
+    @StateObject private var viewModel = AssetsViewModel()
 
     var body: some View {
         List {
@@ -20,31 +17,53 @@ struct AssetsView: View {
                     viewModel.fetchAssets()
                 }
                 .frame(maxWidth: .infinity)
-                .disabled(viewModel.assetsGroup.isEmpty)
+                .disabled(!viewModel.isSearchAllowed)
             } header: {
                 Text(String(localized: "assets_fetch"))
             }
 
-            if viewModel.shouldShowResult {
-                Section {
-                    if viewModel.assets.isEmpty {
-                        Label(String(localized: "assets_not_found"), systemImage: "info.circle.fill")
-                    } else {
-                        ForEach(viewModel.assets) { asset in
-                            NavigationLink {
-                                AssetDetailsView(asset: asset)
-                            } label: {
-                                AssetItemView(asset: asset)
-                            }
-                        }
-                    }
-                } header: {
-                    Text(String(localized: "result"))
+            switch viewModel.viewState {
+            case .idle:
+                EmptyView()
+
+            case .loading:
+                ZStack(alignment: .center) {
+                    ProgressView()
                 }
+                .frame(maxWidth: .infinity)
+                .listRowBackground(Color.clear)
+
+            case let .success(assets):
+                SearchResultView(assets: assets)
+
+            case .failure:
+                Text("ooops")
             }
         }
         .navigationTitle(String(localized: "assets_title"))
         .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+private struct SearchResultView: View {
+    let assets: [NotificareAsset]
+
+    var body: some View {
+        Section {
+            if assets.isEmpty {
+                Label(String(localized: "assets_not_found"), systemImage: "info.circle.fill")
+            } else {
+                ForEach(assets) { asset in
+                    NavigationLink {
+                        AssetDetailsView(asset: asset)
+                    } label: {
+                        AssetItemView(asset: asset)
+                    }
+                }
+            }
+        } header: {
+            Text(String(localized: "result"))
+        }
     }
 }
 
--- Sample/Sample/UI/Assets/AssetsViewModel.swift
+++ Sample/Sample/UI/Assets/AssetsViewModel.swift
@@ -10,28 +10,42 @@ import OSLog
 @MainActor
 class AssetsViewModel: ObservableObject {
     @Published var assetsGroup = ""
-    @Published private(set) var shouldShowResult = false
-    @Published private(set) var assets = [NotificareAsset]()
+    @Published private(set) var viewState: ViewState = .idle
+
+    var isSearchAllowed: Bool {
+        !viewState.isLoading && !assetsGroup.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+    }
 
     func fetchAssets() {
         Logger.main.info("-----> Fetch assets clicked <-----")
-        if !assets.isEmpty {
-            Logger.main.info("-----> Cleaning previously fetched assets <-----")
-            assets.removeAll()
-        }
+        viewState = .loading
 
         Task {
             do {
+                try? await Task.sleep(nanoseconds: 5_000_000_000)
                 let result = try await Notificare.shared.assets().fetch(group: assetsGroup)
+
                 Logger.main.info("-----> Successfully fetched  assets group: \(self.assetsGroup) <-----")
-                assetsGroup = ""
-                assets = result
+                viewState = .success(assets: result)
             } catch {
                 print("-----> Failed to fetch asset group: \(self.assetsGroup): \(error.localizedDescription)")
+                viewState = .failure(error: error)
             }
+        }
+    }
 
-            if !shouldShowResult {
-                shouldShowResult = true
+    enum ViewState {
+        case idle
+        case loading
+        case success(assets: [NotificareAsset])
+        case failure(error: Error)
+
+        var isLoading: Bool {
+            switch self {
+            case .loading:
+                return true
+            default:
+                return false
             }
         }
     }
