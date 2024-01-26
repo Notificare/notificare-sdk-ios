869d32acb491420544620e1d43cf1e844dc92a53
João Ferreira <joao@notifica.re>
2024-01-26 16:46:52 +0000

    refactor(in-app messaging): update events to async

--- NotificareInAppMessagingKit/Sources/NotificareInAppMessaging+Events.swift
+++ NotificareInAppMessagingKit/Sources/NotificareInAppMessaging+Events.swift
@@ -8,25 +8,17 @@ import NotificareKit
 internal extension NotificareEventsModule {
     func logInAppMessageViewed(_ message: NotificareInAppMessage) async throws {
         let this = self as! NotificareInternalEventsModule
-        return try await withCheckedThrowingContinuation { continuation in
-            this.log("re.notifica.event.inappmessage.View", data: ["message": message.id]) { result in
-                continuation.resume(with: result)
-            }
-        }
+        try await this.log("re.notifica.event.inappmessage.View", data: ["message": message.id])
     }
 
     func logInAppMessageActionClicked(_ message: NotificareInAppMessage, action: NotificareInAppMessage.ActionType) async throws {
         let this = self as! NotificareInternalEventsModule
-        return try await withCheckedThrowingContinuation { continuation in
-            this.log(
-                "re.notifica.event.inappmessage.Action",
-                data: [
-                    "message": message.id,
-                    "action": action.rawValue,
-                ]
-            ) { result in
-                continuation.resume(with: result)
-            }
-        }
+        try await this.log(
+            "re.notifica.event.inappmessage.Action",
+            data: [
+                "message": message.id,
+                "action": action.rawValue,
+            ]
+        )
     }
 }
