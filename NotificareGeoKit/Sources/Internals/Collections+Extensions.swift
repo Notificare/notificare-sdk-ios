04bb7709c355002d905e98a000242c000ba90ff0
Helder Pinhal <helder@notifica.re>
2024-02-28 22:31:29 +0000

    refactor(geo): merge array extension files

--- NotificareGeoKit/Sources/Internals/Collections+Extensions.swift
+++ NotificareGeoKit/Sources/Internals/Collections+Extensions.swift
@@ -35,3 +35,25 @@ internal extension Array {
         return copy
     }
 }
+
+internal extension Array {
+    /// Returns a list of `n` evenly spaced elements.
+    ///
+    /// - Parameter n: the number of elements to extract.
+    /// - Returns: A list of `n` evenly spaced elements.
+    func takeEvenlySpaced(_ n: Int) -> Array<Element> {
+        guard n >= 0 else {
+            fatalError("Requested element count \(n) is less than zero.")
+        }
+
+        let interval = (Double(count) - 1) / (Double(n) - 1)
+        var elements: [Element] = []
+
+        for i in 0..<n {
+            let index = Int(round(interval * Double(i)))
+            elements.append(self[index])
+        }
+
+        return elements
+    }
+}
