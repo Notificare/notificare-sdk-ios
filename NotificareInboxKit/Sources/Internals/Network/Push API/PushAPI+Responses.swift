7f79203b94ce0f32d1d7b927aa01f5618c7af582
João Ferreira <joao@notifica.re>
2024-03-26 15:05:54 +0000

    style(inbox): fix linting warnings

--- NotificareInboxKit/Sources/Internals/Network/Push API/PushAPI+Responses.swift
+++ NotificareInboxKit/Sources/Internals/Network/Push API/PushAPI+Responses.swift
@@ -4,10 +4,10 @@
 
 import NotificareKit
 
-internal extension NotificareInternals.PushAPI.Responses {
-    struct RemoteInbox: Decodable {
-        let inboxItems: [NotificareInternals.PushAPI.Models.RemoteInboxItem]
-        let count: Int
-        let unread: Int
+extension NotificareInternals.PushAPI.Responses {
+    internal struct RemoteInbox: Decodable {
+        internal let inboxItems: [NotificareInternals.PushAPI.Models.RemoteInboxItem]
+        internal let count: Int
+        internal let unread: Int
     }
 }
