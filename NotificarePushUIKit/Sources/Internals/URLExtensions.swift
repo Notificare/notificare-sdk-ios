ca954a4088e8bedf477c041ef9af81bf7b597326
yevheniismirnov <yevhenii.smirnov@outlook.com>
2024-06-21 12:28:08 +0200

    chore(push-ui): remove unnecessary import

--- NotificarePushUIKit/Sources/Internals/URLExtensions.swift
+++ NotificarePushUIKit/Sources/Internals/URLExtensions.swift
@@ -2,8 +2,6 @@
 // Copyright (c) 2024 Notificare. All rights reserved.
 //
 
-import NotificareKit
-
 extension URL {
     internal var isHttpUrl: Bool {
         scheme == "http" || scheme == "https"
