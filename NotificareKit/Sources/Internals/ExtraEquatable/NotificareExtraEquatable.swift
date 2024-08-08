fe0c2cf323ffff6477bdc09ba5a23205233e4009
João Ferreira <joao@notifica.re>
2024-08-08 08:39:48 +0100

    refactor(core): add nil support and refactor array and dictionary comparisons

--- NotificareKit/Sources/Internals/ExtraEquatable/NotificareExtraEquatable.swift
+++ NotificareKit/Sources/Internals/ExtraEquatable/NotificareExtraEquatable.swift
@@ -13,9 +13,16 @@ public struct NotificareExtraEquatable<T>: Equatable {
     }
 
     public static func == (lhs: NotificareExtraEquatable<T>, rhs: NotificareExtraEquatable<T>) -> Bool {
+        if let lho = lhs.wrappedValue as? Any?, let rho = rhs.wrappedValue as? Any? {
+            switch (lho, rho) {
+            case (.none, .none):
+                return true
+            default:
+                break
+            }
+        }
+
         switch (lhs.wrappedValue, rhs.wrappedValue) {
-        case is (Void, Void):
-            return true
         case let (lhs as Bool, rhs as Bool):
             return lhs == rhs
         case let (lhs as Int, rhs as Int):
@@ -44,11 +51,49 @@ public struct NotificareExtraEquatable<T>: Equatable {
             return lhs == rhs
         case let (lhs as String, rhs as String):
             return lhs == rhs
-        case let (lhs as [String: NotificareExtraEquatable], rhs as [String: NotificareExtraEquatable]):
-            return lhs == rhs
-        case let (lhs as [NotificareExtraEquatable], rhs as [NotificareExtraEquatable]):
-            return lhs == rhs
+        case let (lhs as [Any], rhs as [Any]):
+            if lhs.isEmpty, rhs.isEmpty {
+                return true
+            }
+
+            if lhs.count != rhs.count {
+                return false
+            }
+
+            for (lha, rha) in zip(lhs, rhs) {
+                let lhv = NotificareExtraEquatable<Any>(wrappedValue: lha)
+                let rhv = NotificareExtraEquatable<Any>(wrappedValue: rha)
+
+                guard lhv == rhv  else {
+                    return false
+                }
+            }
+
+            return true
+        case let (lhs as [String: Any], rhs as [String: Any]):
+            if lhs.isEmpty, rhs.isEmpty {
+                return true
+            }
+
+            if lhs.count != rhs.count {
+                return false
+            }
+
+            for (lhk, lhv) in lhs {
+                guard let rhv = rhs[lhk] else {
+                    return false
+                }
+
+                let lhe = NotificareExtraEquatable<Any>(wrappedValue: lhv)
+                let rhe = NotificareExtraEquatable<Any>(wrappedValue: rhv)
+
+                guard lhe == rhe else {
+                    return false
+                }
+            }
+            return true
         default:
+            NotificareLogger.warning("Unable to compare type provided.")
             return false
         }
     }
