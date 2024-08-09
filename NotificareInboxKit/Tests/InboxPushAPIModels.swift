fa642e51e52b50cf8c4b75c488d7869eb4d2b429
João Ferreira <joao@notifica.re>
2024-08-09 09:18:29 +0100

    test(inbox): simplify models tests

--- NotificareInboxKit/Tests/InboxPushAPIModels.swift
+++ NotificareInboxKit/Tests/InboxPushAPIModels.swift
@@ -6,9 +6,9 @@
 @testable import NotificareInboxKit
 import Testing
 
-struct InboxPushAPIModels {
+internal struct InboxPushAPIModels {
     @Test
-    func testRemoteInboxItemToModel() {
+    internal func testRemoteInboxItemToModel() {
         let expectedItem = NotificareInboxItem(
             id: "testId",
             notification: NotificareNotification(
@@ -21,10 +21,12 @@ struct InboxPushAPIModels {
                 message: "testMessage",
                 content: [],
                 actions: [],
-                attachments: [NotificareNotification.Attachment(
-                    mimeType: "testMimeType",
-                    uri: "testUri"
-                ), ],
+                attachments: [
+                    NotificareNotification.Attachment(
+                        mimeType: "testMimeType",
+                        uri: "testUri"
+                    ),
+                ],
                 extra: ["testKey": "testValue"],
                 targetContentIdentifier: nil
             ),
@@ -51,11 +53,11 @@ struct InboxPushAPIModels {
             expires: Date(timeIntervalSince1970: 1)
         ).toModel()
 
-        assertItem(expectedItem: expectedItem, item: item)
+        #expect(expectedItem == item)
     }
 
     @Test
-    func testRemoteInboxItemWithNullPropsToModel() {
+    internal func testRemoteInboxItemWithNullPropsToModel() {
         let expectedItem = NotificareInboxItem(
             id: "testId",
             notification: NotificareNotification(
@@ -92,41 +94,6 @@ struct InboxPushAPIModels {
             expires: nil
         ).toModel()
 
-        assertItem(expectedItem: expectedItem, item: item)
-    }
-
-    func assertItem(expectedItem: NotificareInboxItem, item: NotificareInboxItem) {
-        #expect(expectedItem.id == item.id)
-        #expect(expectedItem.notification.partial == item.notification.partial)
-        #expect(expectedItem.notification.id == item.notification.id)
-        #expect(expectedItem.notification.type == item.notification.type)
-        #expect(expectedItem.notification.time == item.notification.time)
-        #expect(expectedItem.notification.title == item.notification.title)
-        #expect(expectedItem.notification.subtitle == item.notification.subtitle)
-        #expect(expectedItem.notification.message == item.notification.message)
-        for index in expectedItem.notification.content.indices {
-            #expect(expectedItem.notification.content[index].type == item.notification.content[index].type)
-            #expect(TestUtils.isEqual(type: String.self, a: expectedItem.notification.content[index].data, b: item.notification.content[index].data))
-        }
-        for index in expectedItem.notification.actions.indices {
-            #expect(expectedItem.notification.actions[index].type == item.notification.actions[index].type)
-            #expect(expectedItem.notification.actions[index].label == item.notification.actions[index].label)
-            #expect(expectedItem.notification.actions[index].target == item.notification.actions[index].target)
-            #expect(expectedItem.notification.actions[index].keyboard == item.notification.actions[index].keyboard)
-            #expect(expectedItem.notification.actions[index].camera == item.notification.actions[index].camera)
-            #expect(expectedItem.notification.actions[index].destructive == item.notification.actions[index].destructive)
-            #expect(expectedItem.notification.actions[index].icon?.android == item.notification.actions[index].icon?.android)
-            #expect(expectedItem.notification.actions[index].icon?.ios == item.notification.actions[index].icon?.ios)
-            #expect(expectedItem.notification.actions[index].icon?.web == item.notification.actions[index].icon?.web)
-        }
-        for index in expectedItem.notification.attachments.indices {
-            #expect(expectedItem.notification.attachments[index].mimeType == item.notification.attachments[index].mimeType)
-            #expect(expectedItem.notification.attachments[index].uri == item.notification.attachments[index].uri)
-        }
-        #expect(NSDictionary(dictionary: expectedItem.notification.extra) == NSDictionary(dictionary: item.notification.extra))
-        #expect(expectedItem.notification.targetContentIdentifier == item.notification.targetContentIdentifier)
-        #expect(expectedItem.time == item.time)
-        #expect(expectedItem.opened == item.opened)
-        #expect(expectedItem.expires == item.expires)
+        #expect(expectedItem == item)
     }
 }
--- NotificareInboxKit/Tests/NotificareInboxItemTest.swift
+++ NotificareInboxKit/Tests/NotificareInboxItemTest.swift
@@ -6,9 +6,9 @@
 @testable import NotificareInboxKit
 import Testing
 
-struct NotificareInboxItemTest {
+internal struct NotificareInboxItemTest {
     @Test
-    func testNotificareInboxItemSerialization() {
+    internal func testNotificareInboxItemSerialization() {
         let item = NotificareInboxItem(
             id: "testId",
             notification: NotificareNotification(
@@ -56,14 +56,14 @@ struct NotificareInboxItemTest {
         do {
             let convertedItem = try NotificareInboxItem.fromJson(json: item.toJson())
 
-            assertItem(item: item, convertedItem: convertedItem)
+            #expect(item == convertedItem)
         } catch {
             Issue.record()
         }
     }
 
     @Test
-    func testNotificareInboxItemSerializationWithNilProps() {
+    internal func testNotificareInboxItemSerializationWithNilProps() {
         let item = NotificareInboxItem(
             id: "testId",
             notification: NotificareNotification(
@@ -88,44 +88,9 @@ struct NotificareInboxItemTest {
         do {
             let convertedItem = try NotificareInboxItem.fromJson(json: item.toJson())
 
-            assertItem(item: item, convertedItem: convertedItem)
+            #expect(item == convertedItem)
         } catch {
             Issue.record()
         }
-    }
-
-    func assertItem(item: NotificareInboxItem, convertedItem: NotificareInboxItem) {
-        #expect(item.id == convertedItem.id)
-        #expect(item.notification.partial == convertedItem.notification.partial)
-        #expect(item.notification.id == convertedItem.notification.id)
-        #expect(item.notification.type == convertedItem.notification.type)
-        #expect(item.notification.time == convertedItem.notification.time)
-        #expect(item.notification.title == convertedItem.notification.title)
-        #expect(item.notification.subtitle == convertedItem.notification.subtitle)
-        #expect(item.notification.message == convertedItem.notification.message)
-        for index in item.notification.content.indices {
-            #expect(item.notification.content[index].type == convertedItem.notification.content[index].type)
-            #expect(TestUtils.isEqual(type: String.self, a: item.notification.content[index].data, b: convertedItem.notification.content[index].data))
-        }
-        for index in item.notification.actions.indices {
-            #expect(item.notification.actions[index].type == convertedItem.notification.actions[index].type)
-            #expect(item.notification.actions[index].label == convertedItem.notification.actions[index].label)
-            #expect(item.notification.actions[index].target == convertedItem.notification.actions[index].target)
-            #expect(item.notification.actions[index].keyboard == convertedItem.notification.actions[index].keyboard)
-            #expect(item.notification.actions[index].camera == convertedItem.notification.actions[index].camera)
-            #expect(item.notification.actions[index].destructive == convertedItem.notification.actions[index].destructive)
-            #expect(item.notification.actions[index].icon?.android == convertedItem.notification.actions[index].icon?.android)
-            #expect(item.notification.actions[index].icon?.ios == convertedItem.notification.actions[index].icon?.ios)
-            #expect(item.notification.actions[index].icon?.web == convertedItem.notification.actions[index].icon?.web)
-        }
-        for index in item.notification.attachments.indices {
-            #expect(item.notification.attachments[index].mimeType == convertedItem.notification.attachments[index].mimeType)
-            #expect(item.notification.attachments[index].uri == convertedItem.notification.attachments[index].uri)
-        }
-        #expect(NSDictionary(dictionary: item.notification.extra) == NSDictionary(dictionary: convertedItem.notification.extra))
-        #expect(item.notification.targetContentIdentifier == convertedItem.notification.targetContentIdentifier)
-        #expect(item.time == convertedItem.time)
-        #expect(item.opened == convertedItem.opened)
-        #expect(item.expires == convertedItem.expires)
     }
 }
