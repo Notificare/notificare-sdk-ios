b317d8a59b9f5bef252810888910a57cfa92b6d4
João Ferreira <joao@notifica.re>
2024-08-09 10:08:02 +0100

    test(scannable): simplify models tests

--- NotificareScannablesKit/Tests/NotificareScannableTest.swift
+++ NotificareScannablesKit/Tests/NotificareScannableTest.swift
@@ -6,9 +6,9 @@
 @testable import NotificareKit
 import Testing
 
-struct NotificareScannableTest {
+internal struct NotificareScannableTest {
     @Test
-    func testNotificareScannableSerialization() {
+    internal func testNotificareScannableSerialization() {
         let scannable = NotificareScannable(
             id: "testId",
             name: "testName",
@@ -56,14 +56,14 @@ struct NotificareScannableTest {
         do {
             let convertedScannable = try NotificareScannable.fromJson(json: scannable.toJson())
 
-            assertScannable(scannable: scannable, convertedScannable: convertedScannable)
+            #expect(scannable == convertedScannable)
         } catch {
             Issue.record()
         }
     }
 
     @Test
-    func testNotificareScannableSerializationWithNilProps() {
+    internal func testNotificareScannableSerializationWithNilProps() {
         let scannable = NotificareScannable(
             id: "testId",
             name: "testName",
@@ -75,48 +75,9 @@ struct NotificareScannableTest {
         do {
             let convertedScannable = try NotificareScannable.fromJson(json: scannable.toJson())
 
-            assertScannable(scannable: scannable, convertedScannable: convertedScannable)
+            #expect(scannable == convertedScannable)
         } catch {
             Issue.record()
-        }
-    }
-
-    func assertScannable(scannable: NotificareScannable, convertedScannable: NotificareScannable) {
-        #expect(scannable.id == convertedScannable.id)
-        #expect(scannable.name == convertedScannable.name)
-        #expect(scannable.tag == convertedScannable.tag)
-        #expect(scannable.type == convertedScannable.type)
-        if let notification = scannable.notification,
-           let convertedNotification = convertedScannable.notification
-        {
-            #expect(notification.partial == convertedNotification.partial)
-            #expect(notification.id == convertedNotification.id)
-            #expect(notification.type == convertedNotification.type)
-            #expect(notification.time == convertedNotification.time)
-            #expect(notification.title == convertedNotification.title)
-            #expect(notification.subtitle == convertedNotification.subtitle)
-            #expect(notification.message == convertedNotification.message)
-            for index in notification.content.indices {
-                #expect(notification.content[index].type == convertedNotification.content[index].type)
-                #expect(TestUtils.isEqual(type: String.self, a: notification.content[index].data, b: convertedNotification.content[index].data))
-            }
-            for index in notification.actions.indices {
-                #expect(notification.actions[index].type == convertedNotification.actions[index].type)
-                #expect(notification.actions[index].label == convertedNotification.actions[index].label)
-                #expect(notification.actions[index].target == convertedNotification.actions[index].target)
-                #expect(notification.actions[index].keyboard == convertedNotification.actions[index].keyboard)
-                #expect(notification.actions[index].camera == convertedNotification.actions[index].camera)
-                #expect(notification.actions[index].destructive == convertedNotification.actions[index].destructive)
-                #expect(notification.actions[index].icon?.android == convertedNotification.actions[index].icon?.android)
-                #expect(notification.actions[index].icon?.ios == convertedNotification.actions[index].icon?.ios)
-                #expect(notification.actions[index].icon?.web == convertedNotification.actions[index].icon?.web)
-            }
-            for index in notification.attachments.indices {
-                #expect(notification.attachments[index].mimeType == convertedNotification.attachments[index].mimeType)
-                #expect(notification.attachments[index].uri == convertedNotification.attachments[index].uri)
-            }
-            #expect(NSDictionary(dictionary: notification.extra) == NSDictionary(dictionary: convertedNotification.extra))
-            #expect(notification.targetContentIdentifier == convertedNotification.targetContentIdentifier)
         }
     }
 }
--- NotificareScannablesKit/Tests/ScannablesPushApiModelsTest.swift
+++ NotificareScannablesKit/Tests/ScannablesPushApiModelsTest.swift
@@ -6,9 +6,9 @@
 @testable import NotificareScannablesKit
 import Testing
 
-struct ScannablesPushApiModelsTest {
+internal struct ScannablesPushApiModelsTest {
     @Test
-    func testScannableToModel() {
+    internal func testScannableToModel() {
         let expectedScannable = NotificareScannable(
             id: "testId",
             name: "testName",
@@ -46,7 +46,8 @@ struct ScannablesPushApiModelsTest {
                     NotificareNotification.Attachment(
                         mimeType: "testMimeType",
                         uri: "testUri"
-                    ), ],
+                    ),
+                ],
                 extra: ["testExtraKey": "testExtraValue"],
                 targetContentIdentifier: "testTargetIdentifier"
             )
@@ -98,11 +99,11 @@ struct ScannablesPushApiModelsTest {
             )
         ).toModel()
 
-        assertScannable(expectedScannable: expectedScannable, scannable: scannable)
+        #expect(expectedScannable == scannable)
     }
 
     @Test
-    func testScannableWithNilPropsToModel() {
+    internal func testScannableWithNilPropsToModel() {
         let expectedScannable = NotificareScannable(
             id: "testId",
             name: "testName",
@@ -119,45 +120,6 @@ struct ScannablesPushApiModelsTest {
             data: nil
         ).toModel()
 
-        assertScannable(expectedScannable: expectedScannable, scannable: scannable)
-    }
-
-    func assertScannable(expectedScannable: NotificareScannable, scannable: NotificareScannable) {
-        #expect(expectedScannable.id == scannable.id)
-        #expect(expectedScannable.name == scannable.name)
-        #expect(expectedScannable.tag == scannable.tag)
-        #expect(expectedScannable.type == scannable.type)
-        if let notification = expectedScannable.notification,
-           let convertedNotification = scannable.notification
-        {
-            #expect(notification.partial == convertedNotification.partial)
-            #expect(notification.id == convertedNotification.id)
-            #expect(notification.type == convertedNotification.type)
-            #expect(notification.time == convertedNotification.time)
-            #expect(notification.title == convertedNotification.title)
-            #expect(notification.subtitle == convertedNotification.subtitle)
-            #expect(notification.message == convertedNotification.message)
-            for index in notification.content.indices {
-                #expect(notification.content[index].type == convertedNotification.content[index].type)
-                #expect(TestUtils.isEqual(type: String.self, a: notification.content[index].data, b: convertedNotification.content[index].data))
-            }
-            for index in notification.actions.indices {
-                #expect(notification.actions[index].type == convertedNotification.actions[index].type)
-                #expect(notification.actions[index].label == convertedNotification.actions[index].label)
-                #expect(notification.actions[index].target == convertedNotification.actions[index].target)
-                #expect(notification.actions[index].keyboard == convertedNotification.actions[index].keyboard)
-                #expect(notification.actions[index].camera == convertedNotification.actions[index].camera)
-                #expect(notification.actions[index].destructive == convertedNotification.actions[index].destructive)
-                #expect(notification.actions[index].icon?.android == convertedNotification.actions[index].icon?.android)
-                #expect(notification.actions[index].icon?.ios == convertedNotification.actions[index].icon?.ios)
-                #expect(notification.actions[index].icon?.web == convertedNotification.actions[index].icon?.web)
-            }
-            for index in notification.attachments.indices {
-                #expect(notification.attachments[index].mimeType == convertedNotification.attachments[index].mimeType)
-                #expect(notification.attachments[index].uri == convertedNotification.attachments[index].uri)
-            }
-            #expect(NSDictionary(dictionary: notification.extra) == NSDictionary(dictionary: convertedNotification.extra))
-            #expect(notification.targetContentIdentifier == convertedNotification.targetContentIdentifier)
-        }
+        #expect(expectedScannable == scannable)
     }
 }
