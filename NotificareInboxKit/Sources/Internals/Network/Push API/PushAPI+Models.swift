d1b6cf7c38cf39422437911df9cd9d291712e6cc
João Ferreira <joao@notifica.re>
2024-10-01 12:33:15 +0100

    refactor: remove unused JSON_Decoder

--- NotificareInboxKit/Sources/Internals/Network/Push API/PushAPI+Models.swift
+++ NotificareInboxKit/Sources/Internals/Network/Push API/PushAPI+Models.swift
@@ -57,7 +57,11 @@ extension NotificareInternals.PushAPI.Models.RemoteInboxItem: Decodable {
         subtitle = try container.decodeIfPresent(String.self, forKey: .subtitle)
         message = try container.decode(String.self, forKey: .message)
         attachment = try container.decodeIfPresent(NotificareNotification.Attachment.self, forKey: .attachment)
-        extra = try container.decodeIfPresent([String: Any].self, forKey: .extra) ?? [:]
+        if let extra = try container.decodeIfPresent(NotificareAnyCodable.self, forKey: .extra) {
+            self.extra = extra.value as! [String: Any]
+        } else {
+            extra = [:]
+        }
         opened = try container.decodeIfPresent(Bool.self, forKey: .opened) ?? false
         visible = try container.decodeIfPresent(Bool.self, forKey: .visible) ?? true
         expires = try container.decodeIfPresent(Date.self, forKey: .expires)
